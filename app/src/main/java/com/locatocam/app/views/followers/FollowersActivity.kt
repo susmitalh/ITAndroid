64be7306234f1c76fdf4cb2528648e7dc31ca9d5
jagadish90 <tinkujagadish31@gmail.com>
2022-07-05 18:09:03 +0530

    Setting And Follower

--- app/src/main/java/com/locatocam/app/views/followers/FollowersActivity.kt
+++ app/src/main/java/com/locatocam/app/views/followers/FollowersActivity.kt
@@ -53,11 +53,14 @@ class FollowersActivity : AppCompatActivity(), Follower {
                 when (it.status) {
                     Status.SUCCESS -> {
                     viewmodel.makerequest()
+                        binding.loader.visibility= View.GONE
                     }
                     Status.LOADING -> {
+                        binding.loader.visibility= View.VISIBLE
                         Log.i("ki999","Loading")
                     }
                     Status.ERROR -> {
+                        binding.loader.visibility= View.GONE
                         Log.i("ki999",it.message.toString())
                         Toast.makeText(this@FollowersActivity,it.message,Toast.LENGTH_LONG).show()
                     }
@@ -164,13 +167,13 @@ class FollowersActivity : AppCompatActivity(), Follower {
     }
 
     override fun follow(follow_type: String, follow_process: String, folloedId: Int) {
-        MainActivity.binding.loader.visibility= View.GONE
+        binding.loader.visibility= View.VISIBLE
        postFollow(follow_process,follow_type,folloedId)
 
     }
 
     override fun remove(follow_type: String, follow_process: String, folloedId: Int) {
-        MainActivity.binding.loader.visibility= View.GONE
+        binding.loader.visibility= View.VISIBLE
         postFollow(follow_process,follow_type,folloedId)
     }
      fun postFollow(follow_type: String, follow_process: String, folloedId: Int){
@@ -180,14 +183,14 @@ class FollowersActivity : AppCompatActivity(), Follower {
              viewmodel.follow.collect {
                  when (it.status) {
                      Status.SUCCESS -> {
-                         MainActivity.binding.loader.visibility= View.GONE
+                         binding.loader.visibility= View.GONE
                          setList()
                      }
                      Status.LOADING -> {
-                         MainActivity.binding.loader.visibility= View.VISIBLE
+                         binding.loader.visibility= View.VISIBLE
                      }
                      Status.ERROR -> {
-                         MainActivity.binding.loader.visibility= View.GONE
+                         binding.loader.visibility= View.GONE
                      }
                  }
              }
